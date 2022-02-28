a500dcf2737b21b61329559e2606535bdb83cf5d
adelinka17 <yelemessova.adelina@gmail.com>
2022-02-28 04:17:42 -0500

    Flix App
    Movie Displayer

---
+++ MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Fixter
+//
+//  Created by Adelina Yelemessova on 2/28/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pods/Target Support Files/Pods-Fixter-FixterUITests/Pods-Fixter-FixterUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fixter_FixterUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Fixter_FixterUITests
+@end
